04e624678d39625f8f934e3e24d4c39ae57372d1
Hansuu <130148964+Tamaki1-1@users.noreply.github.com>
2024-04-09 23:38:09 +0900

    Update 43.테스트 프로젝트와 단위 테스트.cs

--- 43.테스트 프로젝트와 단위 테스트.cs
+++ 43.테스트 프로젝트와 단위 테스트.cs
@@ -21,3 +21,136 @@ class 테스트_프로젝트
         Console.WriteLine("Assert.AreNotEqual() - 매개변수 값 2개가 다르면 테스트 통과");
     }
 }
+
+//43-1 테스트 코드 작성 및 실행하기
+
+//MS Test 프로젝트 =>
+//Dul.Tests 프로젝트
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dul.Tests
+{
+    [TestClass]
+    public class AssertDemo
+    {
+        [TestMethod]
+        public void AssertAreEqualTest() //우클릭 후 테스트 실행
+        {
+            //Assert.AreEqual(
+            //  expected: "비교할 첫 번째 개체",
+            //  actual: "비교할 두 번째 개체");
+            Assert.AreEqual(1234, 1234); //테스트 통과
+        }
+    }    
+}
+
+
+//CreatorTest.cs
+using Microsoft.VisualStudio.TestTools.UnitTesting; //테스트 구문
+
+
+namespace Dul.Tests
+{
+    [TestClass]
+    public class CreatorTest
+    {
+        [TestMethod]
+        public void GetNameTest()
+        {
+            // Arrange(정렬), Setup
+            const string expected = "RedPlus";
+
+            // Act(동작), Execute
+            var actual = Creator.GetName();
+
+            // Assert(어설션), Verify
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+
+//Dul.Math.Abs.Test.cs
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dul.Tests.Mathematics
+{
+    [TestClass]
+    public class DulMathTest
+    {
+        [TestMethod]
+        public void AbsTest()
+        {
+            //Arrange, Setup
+            var expected = 1234;
+
+            //Act, Execute
+            var actual = Dul.Math.Abs(1234);
+
+            //Assert, Verify
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+
+//StringLibraryTest.cs
+using Dul;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dul.Tests
+{
+    [TestClass]
+    public class StringLibraryTest
+    {
+        [TestMethod]
+        public void CutStringTest()
+        {
+            // Arrange, Setup
+            var expected = "안녕하...";
+
+            // Act, Execute
+            var actual = "안녕하세요.".CutStringUnicode(6);
+
+            // Assert, Verify
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+
+//DateTimeUtilityTest.cs
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Runtime.Versioning;
+
+namespace Dul.Tests
+{
+    [TestClass]
+    public class DateTimeUtilityTest
+    {
+        [TestMethod]
+        public void ShowTimeOrDateTest()
+        {
+            //(현재 시간 == 오늘 날짜) => 01:01:01 형태
+            var now = DateTime.Now;
+            var expeted = now.ToString("hh:ss:mm");
+
+            //Assert 클래스의 주요 메서드를 사용하여 테스트 통과 확인
+            Assert.AreEqual(expeted, DateTimeUtility.ShowTimeOrDate(now));
+
+        }
+
+        [TestMethod]
+        public void ShowTimeOrDateTestOther()
+        {
+            //(현재 시간 != 오늘 날짜) => 2019-12-20 형태
+            var prev = DateTime.Now.AddDays(-7);
+            //Assert 클래스 없이 직접 실행하는 경우는 테스트 바로 통과
+
+            //yyyy-MM-dd 형태
+            Console.WriteLine(DateTimeUtility.ShowTimeOrDate(prev, "yyyy-mm-dd"));
+
+            //yy-MM-dd 형태
+            Console.WriteLine(DateTimeUtility.ShowTimeOrDate(prev, "yy-MM-dd"));
+        }
+    }
+}
